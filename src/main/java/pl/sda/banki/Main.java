82db0c645ffe5ecdc8b16dc8861b9d91b9cfb74a
SYTEKTOMASZ <59762629+SYTEKTOMASZ@users.noreply.github.com>
2020-03-07 10:46:49 +0100

    Wykonanie zadania

--- src/main/java/pl/sda/banki/Main.java
+++ src/main/java/pl/sda/banki/Main.java
@@ -3,6 +3,25 @@ package pl.sda.banki;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+	BankAccount acc1 = new BankAccount(1,"BankDlaBogatych",5555, 6555);
+	BankAccount acc2 = new BankAccount(2,"BankNowoczesny",4444,6532455);
+	BankAccount acc3 = new BankAccount(3,"Bank",3333,234234);
+	BankAccount acc4 = new BankAccount(4,"BigBank",2222,2342);
+
+	User user1 = new User("Jan", "Kowalski",44,acc1);
+	User user2 = new User("Marcin", "Nowak",66,acc2);
+	User user3 = new User("Katarzyna","Dobroń", 33,acc3);
+	User user4 = new User("Ola", "Lewandowska",33,acc4);
+
+		System.out.println("Imie: "+user1.getName());
+		System.out.println("Naziwsko: "+user1.getLastname());
+		System.out.println("Wiek"+user1.getAge());
+		System.out.println("Nazwa Banku: "+user1.getBank().getName());
+		System.out.println("Numer konta: "+user1.getBank().getAccountNumber());
+		System.out.println("Saldo komnta: "+user1.getBank().getBalance());
+		
+
     }
 }
--- src/main/java/pl/sda/banki/User.java
+++ src/main/java/pl/sda/banki/User.java
@@ -4,5 +4,47 @@ public class User {
     private String name;
     private String lastname;
     private int age;
+    private BankAccount bank;
 
+    public User(String name, String lastname, int age, BankAccount bank) {
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+        this.bank = bank;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public BankAccount getBank() {
+        return bank;
+    }
+
+    public void setBank(BankAccount bank) {
+        this.bank = bank;
+    }
 }
